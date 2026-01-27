c9e8fa84231d444d4c98b0e7d9b57e42afc474d4
nekoemden <nhok158hth@gmail.com>
2026-01-26 16:17:11 -0800

    Added SFX. Fixed memory

--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -3,14 +3,16 @@ class_name Main
 
 const BUBBLE = preload("res://bubble/bubble.tscn")
 @onready var timer: Timer = $Timer
+@onready var sfx: AudioStreamPlayer2D = $Sfx
 
 func _ready() -> void:
 	timer.timeout.connect(on_timed_out)
 
 func spawn_bubble():
-	var bubble: Bubble = BUBBLE.instantiate()
-	bubble.position = Vector2(randi_range(0, get_viewport().size.x), get_viewport().size.y)
-	add_child(bubble)
+		var bubble: Bubble = BUBBLE.instantiate()
+		bubble.position = Vector2(randi_range(0, get_viewport().size.x), get_viewport().size.y +100)
+		bubble.main = self
+		add_child(bubble)
 
 func on_timed_out():
 	spawn_bubble()
--- Code/bubble/bubble.gd
+++ Code/bubble/bubble.gd
@@ -1,21 +1,27 @@
 extends Area2D
 class_name Bubble
 
+var main: Main
 var speed: float = -112 # px/ sec.
 
-func _reandy() -> void:
+func _ready() -> void:
 	input_event.connect(on_clicked)
 
 func _process(delta: float) -> void:
-	position.y += speed * delta 
+		position.y += speed * delta 
+		if position.y < -100:
+			queue_free()
 
 func on_clicked(_viewport, event, _shape):
 	print("hi")
 	if event is InputEventMouseButton:
 		print("A button was clicked")
-	if event.button_index == MOUSE_BUTTON_LEFT:
-		print("It's the Left Button!")
-	if event.pressed:
-		print("Mouse down.")
-	else:
-		print("Mouse up.")
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			print("It's the Left Button!")
+			if event.pressed:
+				print("Mouse down.")
+				main.sfx.play()
+				main.sfx.pitch_scale = randf_range(0.8, 1.2)
+				queue_free()
+			else:
+				print("Mouse up.")
